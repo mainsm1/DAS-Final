320917bc6390b0def18aa26150be1b6659a1f702
Michael Mains <59580219+mainsm1@users.noreply.github.com>
2023-04-03 15:26:45 -0700

    Update DASFinal.R

--- DASFinal.R
+++ DASFinal.R
@@ -12,20 +12,17 @@ DrugDataSetFull <- read_csv("IHME-GBD_2019_DATA-671c0649-1.csv")
 Iso_code_countries <- read_excel("Copy of IHME_GBD_2019_AIR_POLLUTION_1990_2019_ISO3_CODES_Y2021M10D08.xlsx")
 
 DrugDataSetFull <- DrugDataSetFull %>%
-  arrange(desc(measure), location, year, cause)
+  arrange(desc(measure), location, year, age, cause)
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
-ISOCountrys <- left_join(world, Iso_code_countries, by = c("iso_a3" = "ISO3"))
+DrugDataSetFull <- left_join(DrugDataSetFull, Iso_code_countries, by=c('location' = 'location_name'))
 
-DrugDataSetFull <- left_join(ISOCountrys, DrugDataSetFull, by = c("location_name" = "location")) %>%
+DrugDataSetFull <- left_join(world, DrugDataFull, by = c("iso_a3" = "ISO3")) %>%
   select(measure, admin, iso_a3, year, sex, age, cause, val, geometry)
 
 colnames(DrugDataSetFull)[8] = "estimate"
 
-#DrugDataSetFull <- DrugDataSetFull %>%
- # sort(admin, year, age, cause)
+DrugDataSetFull <- DrugDataSetFull %>%
+  arrange(admin, year, desc(age), cause)
 
-unique(DrugDataSetFull$location)
-unique(world$name_long)
-unique(world$admin)
