b7625069d22c3dba14fe540f2a8a3044e0769512
Michael Mains <59580219+mainsm1@users.noreply.github.com>
2023-04-03 15:15:02 -0700

    Update DASFinal.R

--- DASFinal.R
+++ DASFinal.R
@@ -9,15 +9,16 @@ library(readr)
 
 
 DrugDataSetFull <- read_csv("IHME-GBD_2019_DATA-671c0649-1.csv")
+Iso_code_countries <- read_excel("Copy of IHME_GBD_2019_AIR_POLLUTION_1990_2019_ISO3_CODES_Y2021M10D08.xlsx")
 
 DrugDataSetFull <- DrugDataSetFull %>%
   arrange(desc(measure), location, year, cause)
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
-DrugDataSetFull <- left_join(DrugDataSetFull,)
+ISOCountrys <- left_join(world, Iso_code_countries, by = c("iso_a3" = "ISO3"))
 
-DrugDataSetFull <- left_join(world, DrugDataSetFull, by = c("admin" = "location")) %>%
+DrugDataSetFull <- left_join(ISOCountrys, DrugDataSetFull, by = c("location_name" = "location")) %>%
   select(measure, admin, iso_a3, year, sex, age, cause, val, geometry)
 
 colnames(DrugDataSetFull)[8] = "estimate"
