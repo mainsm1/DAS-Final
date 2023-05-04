b3742e52629092e6a092d21ed4fe0ce2a80ae778
Elias Coppock <ewcoppock@gmail.com>
2023-05-03 20:28:02 -0700

    some basic models for alc incidence

--- EliasRScript.R
+++ EliasRScript.R
@@ -1,54 +1,47 @@
 library(tidyverse)
-library(plotly)
-library("rnaturalearth")
-library("rnaturalearthdata")
 library(readr)
 library(stringr)
 
-data <- read_csv("Religion and DrinkAge with ISO3.csv")
-world <- ne_countries(scale = "medium", returnclass = "sf")
-WorldData <- map_data('world') %>% filter(region != "Antarctica") %>% fortify
-
-mapdata <- data %>% left_join(world, by = c("ISO3" = "iso_a3"))
-
-p <- ggplot() +
-  geom_map(data = WorldData, map = WorldData,
-           aes(x = long, y = lat, group = group, map_id=geometry),
-           fill = "white", colour = "#7f7f7f", size=0.5) + 
-  geom_map(data = mapdata, map=WorldData,
-           aes(fill=DrinkingAge, map_id=geometry),
-           colour="#7f7f7f", size=0.5) +
-  coord_map("rectangular", lat0=0, xlim=c(-180,180), ylim=c(-60, 90)) +
-  scale_fill_continuous(low="thistle2", high="darkred", guide="colorbar") +
-  scale_y_continuous(breaks=c()) +
-  scale_x_continuous(breaks=c()) +
-  labs(fill="legend", title="Title", x="", y="") +
-  theme_bw()
-p
-
-mapdata %>% ggplot() +
-  geom_sf(aes(fill=DrinkingAge))
 
 
-
-HYPOTHESIS TESTING BY SEX!!!!
+# HYPOTHESIS TESTING BY SEX (for 2019)!!!
+data <- AgeStandardized_AlcoholCannabis %>% 
+  select(-1,-14) %>%
+  filter(year == 2019) %>%
+  distinct()
   
-ALCOHOL INCIDENCE
+# ALCOHOL INCIDENCE
 females <- data %>% filter(sex == "Female" & measure == "Incidence" & cause == "Alcohol use disorders")
 males <- data %>% filter(sex == "Male" & measure == "Incidence" & cause == "Alcohol use disorders")
 t.test(females$val,males$val)
 
-ALCOHOL DEATHS
+# ALCOHOL DEATHS
 females <- data %>% filter(sex == "Female" & measure == "Deaths" & cause == "Alcohol use disorders")
 males <- data %>% filter(sex == "Male" & measure == "Deaths" & cause == "Alcohol use disorders")
 t.test(females$val,males$val)
 
-CANNABIS INCIDENCE
+# CANNABIS INCIDENCE
 females <- data %>% filter(sex == "Female" & measure == "Incidence" & cause == "Cannabis use disorders")
 males <- data %>% filter(sex == "Male" & measure == "Incidence" & cause == "Cannabis use disorders")
 t.test(females$val,males$val)
 
-CANNABIS DEATHS
-females <- data %>% filter(sex == "Female" & measure == "Deaths" & cause == "Cannabis use disorders")
-males <- data %>% filter(sex == "Male" & measure == "Deaths" & cause == "Cannabis use disorders")
-t.test(females$val,males$val)
+# CANNABIS DEATHS 
+## There are none in the data!
+
+################################################################################
+
+# ALCOHOL INCIDENCE MODEL 
+
+alc.inc.2019 <- data %>%
+  filter(sex == "Both" & measure == "Incidence" & cause == "Alcohol use disorders")
+
+model <- glm(data = alc.inc.2019, val ~ `GDP Measure`)
+summary(model)  
+# Conclusion: no significant relationship between GDP and alcohol incidence (2019)
+
+thing <- alc.inc.2019 %>%
+  left_join(LegalDrinkingAge, by = c("location" = "Country"))
+
+model1 <- glm(data = thing, val ~ DrinkingAge.y)
+summary(model1)  
+# Conclusion: yes significant relationship between drinkage and alcohol incidence (2019)
